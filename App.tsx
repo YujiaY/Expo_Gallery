59889600fb3336fc6880b2ecedae9a540f4fb274
Jack Yuan <Jack@smarte.com.au>
2023-02-16 15:26:54 +1000

    Added two different pages and routing.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
-import React from "react";
-import WebViewWithLink from "./src/web-view-link";
-import {Text} from "react-native";
+import * as React from "react";
+import {Alert, Button, Text, View} from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import WebView from "react-native-webview";
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Text>Home Screen</Text>
+      <Button
+        onPress={() => {
+          Alert.alert("Simple Button pressed");
+          console.warn("Going to web view page...");
+          navigation.navigate("Web AR");
+        }}
+        title="Web View"
+        color="#841584"
+        accessibilityLabel="Going to web view page"
+      />
+    </View>
+  );
+}
+
+function ARScreen() {
+  return (
+    <WebView
+      source={{uri: "https://reactnative.dev/"}}
+      style={{marginBottom: 20, marginTop: 50}}
+    />
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <Text style={{padding: 64, width: "100%"}}>This is the home page.</Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{title: "Overview"}}
+          />
+          <Stack.Screen name="Web AR" component={ARScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
