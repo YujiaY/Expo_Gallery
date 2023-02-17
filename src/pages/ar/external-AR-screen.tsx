5838667a58ab19e2f86b182cfc095e8bd5b8f68d
Jack Yuan <Jack@smarte.com.au>
2023-02-17 16:07:20 +1000

    Update AR&VR Web page.

--- src/pages/ar/external-AR-screen.tsx
+++ src/pages/ar/external-AR-screen.tsx
@@ -4,7 +4,7 @@ import WebView from "react-native-webview";
 const ExternalARScreen = () => {
   return (
     <WebView
-      source={{uri: "https://flat-video-aframe.vercel.app/"}}
+      source={{uri: "https://arvr-examples.vercel.app/"}}
       style={{marginBottom: 20, marginTop: 50}}
     />
   );
