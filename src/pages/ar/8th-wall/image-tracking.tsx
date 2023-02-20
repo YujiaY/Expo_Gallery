14aa74d52e382601dfac30f3789d0fcc789dbf58
Jack Yuan <Jack@smarte.com.au>
2023-02-20 11:37:20 +1000

    Added expo-camera to fix Android not having Camera access pop-up

--- src/pages/ar/8th-wall/image-tracking.tsx
+++ src/pages/ar/8th-wall/image-tracking.tsx
@@ -1,14 +1,35 @@
-import React from "react";
-import WebView from "react-native-webview";
+import React, {useEffect, useState} from "react";
+import {WebView} from "react-native-webview";
+import {Text} from "react-native";
+import {Camera} from "expo-camera";
 
 const ImageTracking = () => {
-  return (
-    <WebView
-      source={{uri: "https://8thwall.8thwall.app/flyer-aframe/"}}
-      style={{marginBottom: 20, marginTop: 50}}
-      allowsInlineMediaPlayback={true}
-    />
-  );
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      // const {status} = await Camera.requestCameraPermissionsAsync();
+      const {status} = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <Text>null access to camera</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera. Please allow access.</Text>;
+  }
+  if (hasPermission) {
+    return (
+      <WebView
+        source={{uri: "https://8thwall.8thwall.app/flyer-aframe/"}}
+        style={{marginBottom: 20, marginTop: 50}}
+        allowsInlineMediaPlayback={true}
+        mediaPlaybackRequiresUserAction={false}
+      />
+    );
+  }
 };
 
 export default ImageTracking;
--- src/pages/ar/8th-wall/pac-man.tsx
+++ src/pages/ar/8th-wall/pac-man.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import WebView from "react-native-webview";
+import {Camera} from "expo-camera";
+import {Text} from "react-native";
 
 const PacMan = () => {
-  return (
-    <WebView
-      source={{
-        uri: "https://toolofnorthamerica.8thwall.app/pizza-hut-pacman-demo/",
-      }}
-      style={{marginBottom: 20, marginTop: 50}}
-      allowsInlineMediaPlayback={true}
-    />
-  );
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      // const {status} = await Camera.requestCameraPermissionsAsync();
+      const {status} = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <Text>null access to camera</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera. Please allow access.</Text>;
+  }
+  if (hasPermission) {
+    return (
+      <WebView
+        source={{
+          uri: "https://toolofnorthamerica.8thwall.app/pizza-hut-pacman-demo/",
+        }}
+        style={{marginBottom: 20, marginTop: 50}}
+        allowsInlineMediaPlayback={true}
+        mediaPlaybackRequiresUserAction={false}
+      />
+    );
+  }
 };
 
 export default PacMan;
